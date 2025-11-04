9df44ff83a8e74d52e8de2ab7464c1defe460009
Mint <demonheart752005@gmail.com>
2025-11-04 20:24:40 +0700

    student entity

---
+++ src/main/java/com/example/final_project/entity/Student.java
@@ -0,0 +1,66 @@
+package com.example.final_project.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Student")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id", nullable = false)
+    private Long studentId;
+
+    @Column(name = "name", nullable = false, length = 100)
+    @NotBlank(message = "Tên học viên không được để trống")
+    private String name;
+
+    @Column(name = "email", length = 100)
+    @Email(message = "Email không hợp lệ")
+    private String email;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name ="last_visit")
+    private LocalDateTime lastVisit;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private StudentStatus status;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        if (status == null) {
+            status = StudentStatus.PENDING;
+        }
+    }
+
+    public enum StudentStatus {
+        PENDING("Đang chờ xác nhận"),
+        ACTIVE("Hoạt động"),
+        LOCKED("Bị khóa");
+
+        private final String displayName;
+
+        StudentStatus(String displayName) {
+            this.displayName = displayName;
+        }
+
+        public String getDisplayName() {
+            return displayName;
+        }
+    }
+}
