f7ab666340e6d76d8654cfb95200cad9773c6cd3
Nhungvu439 <146834474+nhungvu05@users.noreply.github.com>
2025-11-04 23:05:16 +0700

    Tạo nhánh StudentService

---
+++ src/main/java/com/example/final_project/service/StudentService.java
@@ -0,0 +1,19 @@
+package com.example.final_project.service;
+
+import com.example.final_project.entity.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import java.util.Optional;
+
+public interface StudentService {
+
+    Page<Student> searchStudent(String keyword, Pageable pageable);
+
+    Page<Student> findByEmail(String email, Pageable pageable);
+
+    Optional<Student> findById(Long id);
+    Student save(Student teacher);
+    void deleteById(Long id);
+    Optional<Student> findByUsername(String username);
+    Optional<Student> findByEmail(String email);
+}
