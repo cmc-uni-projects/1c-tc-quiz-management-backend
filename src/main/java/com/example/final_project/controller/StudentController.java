e94cc228ed6a2d6c41b854b1654ab3dd9fcdd9e9
Nhungvu439 <146834474+nhungvu05@users.noreply.github.com>
2025-11-06 22:26:11 +0700

    fix StudentController

--- src/main/java/com/example/final_project/controller/StudentController.java
+++ src/main/java/com/example/final_project/controller/StudentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/students")
+@RequestMapping("/students")
 public class StudentController {
 
     @Autowired
