5f7b7abb8b8b2f5b77716f9ab4deae3f2a5265c5
jotoki3142 <shinaki3142@gmail.com>
2025-11-12 00:07:10 +0700

    Danh muc admin

--- src/main/java/com/example/final_project/entity/Category.java
+++ src/main/java/com/example/final_project/entity/Category.java
@@ -30,6 +30,9 @@ public class Category {
     @Column(nullable = false)
     private String createdBy; // Stores the email of the creator (Admin or Teacher)
 
+    @Transient
+    private String createdByRole; // transient field for UI: "admin" | "teacher"
+
 //    @OneToMany(mappedBy = "category", cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
 //    private List<Question> questions = new ArrayList<>();
 
