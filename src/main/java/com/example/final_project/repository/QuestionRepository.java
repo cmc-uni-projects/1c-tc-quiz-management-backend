3becb8caa5f9d096b7b1a984380845ba9b62d18e
Hieu Bui Quang Trung <BIT230156@st.cmcu.edu.vn>
2025-12-01 01:44:46 +0700

    fixBEcauhoi

--- src/main/java/com/example/final_project/repository/QuestionRepository.java
+++ src/main/java/com/example/final_project/repository/QuestionRepository.java
@@ -1,10 +1,16 @@
 package com.example.final_project.repository;
 
+import com.example.final_project.dto.QuestionResponseDto;
 import com.example.final_project.entity.Question;
 import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    static Page<QuestionResponseDto> searchAndFilter(String keyword, String difficulty, String type, Long categoryId, String createdBy, Pageable pageable) {
+        return null;
+    }
+
+
     Page<Question> findAllByOrderByCreatedAtDesc(Pageable pageable);
     Page<Question> findByCreatedByOrderByCreatedAtDesc(String createdBy, Pageable pageable);
 }
